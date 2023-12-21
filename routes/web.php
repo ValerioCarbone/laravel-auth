e9058a3de5303880c3445b14ea35b213012828d3
ValerioCarbone <valeriocarbone10@gmail.com>
2023-12-21 19:00:12 +0100

    CHANGE - Dashb & Project Cntrllr folder. ADD - admin index

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -30,10 +27,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth', 'verified'])->name('admin')->prefix('admin.')->group(
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(
     function () {
+
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-        Route::resource('admin', ProjectController::class);
+
+        Route::resource('projects', ProjectController::class);
     }
 
 );
